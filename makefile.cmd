69f03ba7b0af71fabdc3b0fc8f1c95e7b9c4f72f
yoshow <ruanyu@live.com>
2014-10-08 23:11:17 +0800

    Update build files

--- makefile.cmd
+++ makefile.cmd
@@ -1,3 +1,27 @@
 @echo off
 
-nant -buildfile:default.build
+@rem set /p targetFramework=Please Enter(net-3.5 OR net-4.0 OR mono-3.5)£º
+set targetFramework=%~1
+
+if "%targetFramework%" equ "net-3.5" goto NET35
+if "%targetFramework%" equ "net-4.0" goto NET40
+if "%targetFramework%" equ "mono-3.5" goto MONO35
+if "%targetFramework%" equ "mono-4.0" goto MONO40
+goto DEFAULT
+
+:DEFAULT
+nant -buildfile:default.build -logfile:default.build.log.txt
+goto end
+:NET35
+nant -buildfile:default.build -t:%targetFramework% -logfile:default_build_log.txt
+goto end
+:NET40
+nant -buildfile:default.build -t:%targetFramework% -logfile:default_build_log.txt
+goto end
+:MONO35
+nant -buildfile:default.build -t:%targetFramework% -logfile:default_build_log.txt
+goto end
+:MONO40
+nant -buildfile:default.build -t:%targetFramework% -logfile:default_build_log.txt
+goto end
+:END
