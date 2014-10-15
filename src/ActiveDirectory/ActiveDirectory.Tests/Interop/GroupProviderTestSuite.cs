5bbf3525a5bb30e2dbe463871e0ff1e5b1cc8730
yoshow <ruanyu@live.com>
2014-10-15 14:54:56 +0800

    Init Release

---
+++ src/ActiveDirectory/ActiveDirectory.Tests/Interop/GroupProviderTestSuite.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Diagnostics;
+using System.Data.Common;
+using System.IO;
+
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Elane.X.ActiveDirectory.Interop.TestSuite
+{
+    /// <summary>
+    /// ActiveDirectorySynchronizerTestSuite 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class GroupProviderTestSuite
+    {
+        [TestMethod]
+        public void TestGetGroupsByName()
+        {
+            string groupName = "集团总部";
+
+            string[] groups = ActiveDirectoryManagement.Instance.User.GetUsersByGroupName(groupName);
+
+            Assert.IsNotNull(groups);
+        }
+
+
+        [TestMethod]
+        public void TestCreateGroupsByLoginName()
+        {
+            string groupName = "集团信息中心";
+
+            //provider.Group.Add(groupName,);
+
+            //Assert.IsNotNull(groups);
+        }
+    }
+}
