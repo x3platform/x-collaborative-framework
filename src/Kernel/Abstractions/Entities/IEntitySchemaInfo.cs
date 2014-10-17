3b49b3a6ccd95a8197e51694453653177d07af0c
yoshow <ruanyu@live.com>
2014-10-18 00:59:31 +0800

    Init Release

--- src/Kernel/Abstractions/Entities/IEntitySchemaInfo.cs
+++ src/Kernel/Abstractions/Entities/IEntitySchemaInfo.cs
@@ -1,6 +1,10 @@
 namespace X3Platform.Entities
 {
+<<<<<<< HEAD
     /// <summary>实体对象架构信息接口</summary>
+=======
+    /// <summary>实体架构信息接口</summary>
+>>>>>>> 58ed63ff8ace2b23714ed4928a94b727f28c707e
     public interface IEntitySchemaInfo
     {
     }
