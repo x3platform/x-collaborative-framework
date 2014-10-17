b17f6eb75fe10f7257762a5c913083059b1c08d9
yoshow <ruanyu@live.com>
2014-10-18 00:59:31 +0800

    Init Release

--- src/Kernel/Abstractions/Entities/IEntityLifeHistoryInfo.cs
+++ src/Kernel/Abstractions/Entities/IEntityLifeHistoryInfo.cs
@@ -1,6 +1,10 @@
 namespace X3Platform.Entities
 {
+<<<<<<< HEAD
     /// <summary>实体对象生命周期</summary>
+=======
+    /// <summary>实体类生命周期</summary>
+>>>>>>> 58ed63ff8ace2b23714ed4928a94b727f28c707e
     public interface IEntityLifeHistoryInfo
     {
     }
