38c21e6f95c55f27c6b49c730bcb3b7658f66f09
yoshow <ruanyu@live.com>
2014-11-04 21:19:53 +0800

    更新内置默认网站

---
+++ src/WebSite/1.0.0/resources/javascript/baidu-tongji.js
@@ -0,0 +1,23 @@
+﻿setTimeout(function()
+{
+    try
+    {
+        // 百度统计代码
+        var tongjiToken = '50ec37835e145af07441db70959c8575'; // => x3platform.com
+        var tongji = document.createElement('script');
+        tongji.type = 'text/javascript';
+        tongji.async = true;
+        tongji.src = (('https:' == document.location.protocol) ? 'https://' : 'http://') + 'hm.baidu.com/h.js?' + tongjiToken;
+
+        var script = document.getElementsByTagName('script')[0];
+
+        script.parentNode.insertBefore(tongji, script);
+    }
+    catch (ex)
+    {
+        if (typeof (console) !== 'undefined')
+        {
+            console.error(ex);
+        }
+    }
+}, 1000);
