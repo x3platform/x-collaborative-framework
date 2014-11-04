fc39733e9b392f0ae523ff9e7ef7c6985952f9a8
yoshow <ruanyu@live.com>
2014-11-04 21:47:27 +0800

    更新 Web 功能

---
+++ src/Web/Web.Mixed/Mvc/Controllers/SDKController.cs
@@ -0,0 +1,17 @@
+﻿namespace X3Platform.Web.Mvc.Controllers
+{
+    using System.Web.Mvc;
+
+    /// <summary>在线开发辅助工具包</summary>
+    public sealed partial class SDKController : Controller
+    {
+        #region 函数:Index()
+        /// <summary>主页</summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View("/views/main/sdk/default.cshtml");
+        }
+        #endregion
+    }
+}
