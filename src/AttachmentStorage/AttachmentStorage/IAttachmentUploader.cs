34973378f9e8d5039a650cd3d03f7dd3bf1a093c
yoshow <ruanyu@live.com>
2015-01-30 11:46:46 +0800

    添加 AttachmentUploader

---
+++ src/AttachmentStorage/AttachmentStorage/IAttachmentUploader.cs
@@ -0,0 +1,13 @@
+﻿namespace X3Platform.AttachmentStorage
+{
+    #region Using Libraries
+    using System;
+    using System.Web;
+    #endregion
+
+    /// <summary>附件父级对象</summary>
+    public interface IAttachmentUploader
+    {
+        int Upload(HttpContext context);
+    }
+}
