555dd121b5502082e137e8bc9ade469aa2c9c693
yoshow <ruanyu@live.com>
2015-05-13 11:26:22 +0800

    Fix build

--- src/Connect/Connect.Mixed/Properties/AssemblyInfo.cs
+++ src/Connect/Connect.Mixed/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Connect.Mixed")]
-[assembly: AssemblyDescription("应用连接器管理扩展")]
+[assembly: AssemblyDescription("应用连接器管理混合扩展")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
