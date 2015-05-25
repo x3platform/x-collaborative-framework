6b0ffcfb87c1c247bb69e6173e7898b287308e8d
yoshow <ruanyu@live.com>
2015-05-25 09:57:22 +0800

    2015-05-25 update

--- src/Sessions/Sessions.Tests/SessionContextTests.cs
+++ src/Sessions/Sessions.Tests/SessionContextTests.cs
@@ -1,30 +1,21 @@
-namespace X3Platform.Email.Client.TestSuite
+namespace X3Platform.Sessions.Tests
 {
-    using System;
+  using System;
+  using System.Configuration;
+  using System.Diagnostics;
 
-    using System.Diagnostics;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using X3Platform.Sessions.Configuration;
+  using X3Platform.Sessions;
 
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using System.Configuration;
-    using X3Platform.Sessions.Configuration;
-    using X3Platform.Sessions;
-
-    [TestClass]
-    public class SessionsContextTestSuite
+  [TestClass]
+  public class SessionsContextTests
+  {
+    /// <summary></summary>
+    [TestMethod]
+    public void TestLoad()
     {
-        string fullConfigPath = ConfigurationManager.AppSettings["fullConfigPath"];
-
-        [TestInitialize()]
-        public void Initialize()
-        {
-             // SessionsConfigurationView.Instance.LoadInstance(fullConfigPath);
-        }
-
-        /// <summary></summary>
-        [TestMethod]
-        public void TestLoad()
-        {
-            Assert.IsNotNull(SessionContext.Instance.AccountCacheService);
-        }
+      Assert.IsNotNull(SessionContext.Instance.AccountCacheService);
     }
+  }
 }
