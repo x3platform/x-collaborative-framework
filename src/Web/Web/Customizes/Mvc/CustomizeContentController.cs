db2d3c262cffa0d8c7b0043687a024b0c7a664db
yoshow <ruanyu@live.com>
2015-06-04 09:01:53 +0800

    add files

---
+++ src/Web/Web/Customizes/Mvc/CustomizeContentController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+
+namespace X3Platform.Web.Customizes.Mvc.Controllers
+{
+  using X3Platform.Apps;
+  using X3Platform.Apps.Model;
+  using X3Platform.Web.Configuration;
+  using X3Platform.Web.Mvc.Attributes;
+  using X3Platform.Web.Mvc.Controllers;
+
+  /// <summary>自定义内容信息</summary>
+  public sealed class CustomizeContentController : CustomController
+  {
+    #region 函数:Index()
+    /// <summary>主页</summary>
+    /// <returns></returns>
+    [LoginFilter]
+    public ActionResult Index()
+    {
+      // 所属应用信息
+      ApplicationInfo application = ViewBag.application = AppsContext.Instance.ApplicationService[WebConfiguration.APP_NAME_CUSTOMIZES];
+
+      // -------------------------------------------------------
+      // 身份验证
+      // -------------------------------------------------------
+
+      if (!AppsSecurity.IsAdministrator(KernelContext.Current.User, application.ApplicationName))
+      {
+        ApplicationError.Write(401);
+      }
+
+      return View("/views/main/customizes/customize-content-list.cshtml");
+    }
+    #endregion
+  }
+}
---
+++ src/Web/Web/Customizes/Mvc/CustomizePageController.cs
@@ -0,0 +1,35 @@
+﻿namespace X3Platform.Web.Customizes.Mvc.Controllers
+{
+  using System.Web.Mvc;
+  using X3Platform.Apps;
+  using X3Platform.Apps.Model;
+  using X3Platform.Web.Configuration;
+  using X3Platform.Web.Mvc.Attributes;
+  using X3Platform.Web.Mvc.Controllers;
+
+  /// <summary>自定义内容信息</summary>
+  public sealed class CustomizePageController : CustomController
+  {
+    #region 函数:List()
+    /// <summary>列表</summary>
+    /// <returns></returns>
+    [LoginFilter]
+    public ActionResult List()
+    {
+      // 所属应用信息
+      ApplicationInfo application = ViewBag.application = AppsContext.Instance.ApplicationService[WebConfiguration.APP_NAME_CUSTOMIZES];
+
+      // -------------------------------------------------------
+      // 身份验证
+      // -------------------------------------------------------
+
+      if (!AppsSecurity.IsAdministrator(KernelContext.Current.User, application.ApplicationName))
+      {
+        ApplicationError.Write(401);
+      }
+
+      return View("/views/main/customizes/customize-page-list.cshtml");
+    }
+    #endregion
+  }
+}
