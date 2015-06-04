b9359519bb9c965ce4e74ab9699b06b7076881a0
yoshow <ruanyu@live.com>
2015-06-04 09:57:12 +0800

    fixed tests

--- src/Web/Web.Tests/APIs/Configuration/APIsConfigurationTests.cs
+++ src/Web/Web.Tests/APIs/Configuration/APIsConfigurationTests.cs
@@ -1,28 +1,28 @@
 namespace X3Platform.Web.Tests.APIs.Configuration
 {
-    using System;
-    using System.Xml;
+  using System;
+  using System.Xml;
 
-    using Common.Logging;
+  using Common.Logging;
 
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-    using X3Platform.Ajax;
-    using X3Platform.Util;
+  using X3Platform.Ajax;
+  using X3Platform.Util;
 
-    using X3Platform.Web.APIs.Configuration;
+  using X3Platform.Web.APIs.Configuration;
 
-    /// <summary>≤‚ ‘ Web APIs ≈‰÷√</summary>
-    [TestClass]
-    public class APIsConfigurationTests
+  /// <summary>≤‚ ‘ Web APIs ≈‰÷√</summary>
+  [TestClass]
+  public class APIsConfigurationTests
+  {
+    /// <summary>≤‚ ‘Œƒº˛</summary>
+    [TestMethod]
+    public void TestLoad()
     {
-        /// <summary>≤‚ ‘Œƒº˛</summary>
-        [TestMethod]
-        public void TestLoad()
-        {
-            APIsConfiguration configuration = APIsConfigurationView.Instance.Configuration;
+      APIsConfiguration configuration = APIsConfigurationView.Instance.Configuration;
 
-            Assert.IsNotNull(configuration);
-        }
+      Assert.IsNotNull(configuration);
     }
+  }
 }
