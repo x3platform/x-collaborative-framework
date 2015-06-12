453ac2483ca8ecb7eed6758485ef6fb55032d7ca
yoshow <ruanyu@live.com>
2015-06-12 15:29:08 +0800

    Add CustomizeContentWidget.cs

---
+++ src/Web/Web/Customizes/Widgets/CustomizeContentWidget.cs
@@ -0,0 +1,30 @@
+namespace X3Platform.Web.Customizes.Widgets
+{
+  #region Using Libraries
+  using System;
+
+  using X3Platform.Util;
+  using X3Platform.Velocity;
+  using X3Platform.Web.Configuration;
+  #endregion
+
+  /// <summary>自定义内容的窗口部件</summary>
+  public sealed class CustomizeContentWidget : AbstractWidget
+  {
+    /// <summary></summary>
+    public override string ParseHtml()
+    {
+      string widgetRuntimeId = StringHelper.ToGuid();
+
+      VelocityContext context = new VelocityContext();
+
+      context.Put("widgetRuntimeId", widgetRuntimeId);
+
+      context.Put("height", (this.Height == 0 ? "height:auto;" : "height:" + this.Height + "px;"));
+      context.Put("width", (this.Width == 0 ? "width:auto;" : "width:" + this.Width + "px;"));
+      context.Put("contentName", this.options["contentName"]);
+
+      return VelocityManager.Instance.Merge(context, "themes/" + WebConfigurationView.Instance.ThemeName + "/widgets/customize-content.vm");
+    }
+  }
+}
