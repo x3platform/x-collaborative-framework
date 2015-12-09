11d8f506b666c89c68912c065abb4990dd1b6e21
yoshow <ruanyu@live.com>
2015-12-09 14:33:24 +0800

    Add file

---
+++ src/Kernel/Abstractions/Security/VerificationCode/VerificationObjectType.cs
@@ -0,0 +1,16 @@
+namespace X3Platform.Security.VerificationCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>验证对象类型</summary>
+    public enum VerificationObjectType
+    {
+        /// <summary>手机</summary>
+        Mobile,
+
+        /// <summary>邮件</summary>
+        Mail
+    }
+}
