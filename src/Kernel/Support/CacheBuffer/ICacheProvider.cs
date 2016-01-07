3b0205dc7f0167159fa8a9e1ae7dbce999c140dc
yoshow <ruanyu@live.com>
2016-01-07 15:09:30 +0800

    CacheBuffer update

--- src/Kernel/Support/CacheBuffer/ICacheProvider.cs
+++ src/Kernel/Support/CacheBuffer/ICacheProvider.cs
@@ -8,46 +8,39 @@ namespace X3Platform.CacheBuffer
     {
         #region 属性:索引
         /// <summary>索引</summary>
-        object this[string key] { get; set; }
+        object this[string name] { get; set; }
         #endregion
 
-        #region 函数:Contains(string key)
-        /// <summary>
-        /// 
-        /// </summary>
+        #region 函数:Contains(string name)
+        /// <summary></summary>
         /// <param name="key"></param>
         /// <returns></returns>
-        bool Contains(string key);
+        bool Contains(string name);
         #endregion
 
-        #region 函数:Read(string key)
+        #region 函数:Get(string name)
         ///<summary>读取缓存记录</summary>
-        ///<param name="key">标识</param>
+        ///<param name="name">名称</param>
         ///<returns>返回缓存对象的详细信息</returns>
-        object Read(string key);
+        object Get(string name);
         #endregion
 
-        #region 函数:Write(string key, object value)
+        #region 函数:Add(string name, object value)
         ///<summary>写入缓存记录</summary>
         ///<param name="key">标志</param>
         ///<param name="value">RuanYu.CacheBuffer.Model.TaskInfo Id号</param>
-        void Write(string key, object value);
+        void Add(string name, object value);
         #endregion
 
-        #region 函数:Write(string key, object value, int minutes)
-        void Write(string key, object value, int minutes);
+        #region 函数:Add(string name, object value, int minutes)
+        void Add(string name, object value, int minutes);
         #endregion
 
-        #region 函数:Delete(string key)
+        #region 函数:Remove(string name)
         ///<summary>查询某条记录</summary>
         ///<param name="Id">RuanYu.CacheBuffer.Model.TaskInfo Id号</param>
         ///<returns>返回一个 RuanYu.CacheBuffer.Model.TaskInfo 实例的详细信息</returns>
-        void Delete(string key);
-        #endregion
-
-        #region 函数:Clear()
-        ///<summary>清空数据</summary>
-        void Clear();
+        void Remove(string name);
         #endregion
     }
 }
---
+++ src/WebSite/1.0.0/views/main/account/set-password.js
@@ -0,0 +1,66 @@
+﻿(function(x, document)
+{
+    var main = {
+        setPassword: function()
+        {
+            var email = x.dom('#email').val();
+            var code = x.dom('#code').val();
+
+            var originalPassword = x.dom('#originalPassword').val();
+
+            x.dom('#password').val(CryptoJS.SHA1(originalPassword).toString());
+
+            if(email == '' || code == '')
+            {
+                x.msg('必须填写电子邮箱地址和验证码。');
+                return;
+            }
+
+            var code = x.dom('#code').val();
+
+            if(originalPassword.length < 7)
+            {
+                x.msg('密码长度必须大于八位。');
+                return;
+            }
+
+            if(originalPassword != x.dom('#confirmPassword').val())
+            {
+                x.dom('#originalPassword').val('');
+                x.dom('#confirmPassword').val('');
+
+                x.msg('密码和确认密码不匹配，请重新输入。');
+                return;
+            }
+
+            x.dom('.loading').css({ 'display': '' });
+
+            x.net.xhr('/api/hr.general.setPassword.aspx', x.dom.data.serialize(), function(response)
+            {
+                var result = x.toJSON(response).message;
+
+                switch(Number(result.returnCode))
+                {
+                    case 0:
+                        x.msg(result.value);
+                        break;
+
+                    case 1:
+                        x.msg(result.value);
+                        break;
+                }
+
+                x.dom('.loading').css({ 'display': 'none' });
+            });
+        }
+    };
+
+    x.dom.ready(function()
+    {
+        // -------------------------------------------------------
+        // 绑定事件
+        // -------------------------------------------------------
+
+        x.dom('#btnSubmit').on('click', main.setPassword);
+    });
+})(x, document);
