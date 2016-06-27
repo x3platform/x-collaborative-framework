d6f6eff5a01c3c1d9d556a964b3c4e410c5b0e4a
yoshow <ruanyu@live.com>
2016-06-27 16:47:37 +0800

    fix(Web):fix some bugs

--- src/WebSite/1.0.0/views/main/connect/connect-form.js
+++ src/WebSite/1.0.0/views/main/connect/connect-form.js
@@ -2,41 +2,48 @@
 
 main.connect.form = {
 
-  /*#region 函数:save()*/
-  save: function()
-  {
-    if(!x.dom.data.check())
+    /*#region 函数:save()*/
+    save: function()
     {
-      var outString = '<?xml version="1.0" encoding="utf-8"?>';
-
-      outString += '<request>';
-      outString += x.dom.data.serialize({ storageType: 'xml', includeRequestNode: false });
-      outString += '</request>';
-
-      x.net.xhr('/api/connect.save.aspx', outString, {
-        waitingMessage: i18n.strings.msg_net_waiting_save_tip_text,
-        callback: function(response)
+        if (!x.dom.data.check())
         {
-          // todo:
+            var outString = '<?xml version="1.0" encoding="utf-8"?>';
+
+            outString += '<request>';
+            outString += x.dom.data.serialize({ storageType: 'xml', includeRequestNode: false });
+            outString += '</request>';
+
+            x.net.xhr('/api/connect.save.aspx', outString, {
+                waitingMessage: i18n.strings.msg_net_waiting_save_tip_text,
+                callback: function(response)
+                {
+                    if ($('#applicationName').val() == 'PersonalSettings')
+                    {
+                        location.href = '/account/settings/applications';
+                    }
+                    else
+                    {
+                        history.back();
+                    }
+                }
+            });
         }
-      });
+    },
+    /*#endregion*/
+
+    /*#region 函数:load()*/
+    /*
+    * 页面加载事件
+    */
+    load: function()
+    {
+        // -------------------------------------------------------
+        // 绑定事件
+        // -------------------------------------------------------
+
+        // [空]
     }
-  },
-  /*#endregion*/
-
-  /*#region 函数:load()*/
-  /*
-  * 页面加载事件
-  */
-  load: function()
-  {
-    // -------------------------------------------------------
-    // 绑定事件
-    // -------------------------------------------------------
-
-    // [空]
-  }
-  /*#endregion*/
+    /*#endregion*/
 };
 
 $(document).ready(main.connect.form.load);
