d2f23e15781402dc53e33d49f30d7e8a1603afd7
yoshow <ruanyu@live.com>
2016-06-27 16:41:55 +0800

    feat(Web.API): update WebServiceMethod.

--- src/Web/Web.APIs/Methods/WebServiceMethod.cs
+++ src/Web/Web.APIs/Methods/WebServiceMethod.cs
@@ -17,6 +17,8 @@ namespace X3Platform.Web.APIs.Methods
         protected string url;
         /// <summary>方法名称</summary>
         protected string methodName;
+        /// <summary>方法参数类型</summary>
+        protected string paramType = "xml";
         /// <summary>方法参数数量</summary>
         protected int paramCount = -1;
         /// <summary>方法返回类型</summary>
@@ -40,6 +42,11 @@ namespace X3Platform.Web.APIs.Methods
 
             this.methodName = this.options["methodName"];
 
+            if (this.options.ContainsKey("paramType"))
+            {
+                this.paramType = this.options["paramType"];
+            }
+
             if (this.options.ContainsKey("paramCount"))
             {
                 this.paramCount = Convert.ToInt32(this.options["paramCount"]);
@@ -61,42 +68,50 @@ namespace X3Platform.Web.APIs.Methods
             // 设置映射参数
             Mapping();
 
-            // 构造参数
-            List<object> list = new List<object>();
-
-            XmlNodeList nodes = doc.DocumentElement.ChildNodes;
-
-            foreach (XmlNode node in nodes)
+            if (paramType == "xml")
             {
-                if (node.Name == "query")
-                    continue;
-
-                list.Add(node.InnerText);
-
-                if (paramCount != -1 && list.Count >= paramCount) { break; }
-            }
-
-            if (paramCount == 0)
-            {
-                list.Clear();
-            }
-
-            if (this.returnType == "bool")
-            {
-                list.Add(false);
-                list.Add(false);
-
-                object[] args = list.ToArray();
-
                 // 执行方法
-                WebServiceHelper.Invoke(this.url, this.methodName, args);
-
-                return args[args.Length - 1];
+                return WebServiceHelper.Invoke(this.url, this.methodName, new object[] { doc });
             }
             else
             {
-                // 执行方法
-                return WebServiceHelper.Invoke(this.url, this.methodName, list.ToArray());
+                // 构造参数
+                List<object> list = new List<object>();
+
+                XmlNodeList nodes = doc.DocumentElement.ChildNodes;
+
+                foreach (XmlNode node in nodes)
+                {
+                    if (node.Name == "query")
+                        continue;
+
+                    list.Add(node.InnerText);
+
+                    if (paramCount != -1 && list.Count >= paramCount) { break; }
+                }
+
+                if (paramCount == 0)
+                {
+                    list.Clear();
+                }
+
+                if (this.returnType == "bool")
+                {
+                    list.Add(false);
+                    list.Add(false);
+
+                    object[] args = list.ToArray();
+
+                    // 执行方法
+                    WebServiceHelper.Invoke(this.url, this.methodName, args);
+
+                    return args[args.Length - 2];
+                }
+                else
+                {
+                    // 执行方法
+                    return WebServiceHelper.Invoke(this.url, this.methodName, list.ToArray());
+                }
             }
         }
     }
