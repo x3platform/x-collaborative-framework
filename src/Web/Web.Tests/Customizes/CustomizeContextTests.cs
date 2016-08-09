32d8fc77fa54ba5710e0644e198014ac0034ba14
yoshow <ruanyu@live.com>
2016-08-09 17:31:09 +0800

    fix(Web):build

--- src/Web/Web.Tests/Customizes/CustomizeContextTests.cs
+++ src/Web/Web.Tests/Customizes/CustomizeContextTests.cs
@@ -1,28 +1,28 @@
 ﻿namespace X3Platform.Web.Tests.Customize
 {
-  using System;
-  using System.Text;
-  using System.Collections.Generic;
-  using System.Diagnostics;
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Diagnostics;
 
-  using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-  using X3Platform.Web.Customizes;
+    using X3Platform.Web.Customizes;
 
-  /// <summary></summary>
-  [TestClass]
-  public class CustomizeContextTests
-  {
-    [TestMethod]
-    public void TestRestart()
+    /// <summary></summary>
+    [TestClass]
+    public class CustomizeContextTests
     {
-      CustomizeContext.Instance.Restart();
+        [TestMethod]
+        public void TestRestart()
+        {
+            CustomizeContext.Instance.Restart();
 
-      Assert.IsNotNull(CustomizeContext.Instance.CustomizePageService);
-      Assert.IsNotNull(CustomizeContext.Instance.CustomizeContentService);
-      Assert.IsNotNull(CustomizeContext.Instance.CustomizeLayoutService);
-      Assert.IsNotNull(CustomizeContext.Instance.CustomizeWidgetService);
-      Assert.IsNotNull(CustomizeContext.Instance.CustomizeWidgetInstanceService);
+            Assert.IsNotNull(CustomizeContext.Instance.CustomizePageService);
+            Assert.IsNotNull(CustomizeContext.Instance.CustomizeContentService);
+            Assert.IsNotNull(CustomizeContext.Instance.CustomizeLayoutService);
+            Assert.IsNotNull(CustomizeContext.Instance.CustomizeWidgetService);
+            Assert.IsNotNull(CustomizeContext.Instance.CustomizeWidgetInstanceService);
+        }
     }
-  }
 }
